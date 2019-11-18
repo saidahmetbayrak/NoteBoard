326f9d4d14d989df18ab5163915086350a198e17
said ahmet bayrak <saidahmetbayrak@hotmail.com>
2019-11-18 14:42:09 +0300

    version 1.0.1

---
+++ NoteBoard/NoteBoard.BLL/NoteController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using NoteBoard.DAL.Repositories;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBoard.BLL
+{
+    class NoteController
+    {
+        NoteDAL _noteDAL;
+        public NoteController()
+        {
+            _noteDAL = new NoteDAL();
+        }
+
+        public void a()
+        {
+            int a = 4;  
+        }
+    }
+}
---
+++ NoteBoard/NoteBoard.BLL/PasswordController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using NoteBoard.DAL.Repositories;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBoard.BLL
+{
+    class PasswordController
+    {
+        PasswordDAL _passwordDAL;
+        public PasswordController()
+        {
+            _passwordDAL = new PasswordDAL();
+        }
+    }
+}
