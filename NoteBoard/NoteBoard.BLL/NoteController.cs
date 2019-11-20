9323b07cf5b61346248321f4aa13020ca4ad5e8a
said ahmet bayrak <saidahmetbayrak@hotmail.com>
2019-11-20 09:35:25 +0300

    version 1.1.0

--- NoteBoard/NoteBoard.BLL/NoteController.cs
+++ NoteBoard/NoteBoard.BLL/NoteController.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using NoteBoard.DAL.Repositories;
+using NoteBoard.Model;
+using System;
 using System.Collections.Generic;
-using NoteBoard.DAL.Repositories;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,32 @@ namespace NoteBoard.BLL
         {
             _noteDAL = new NoteDAL();
         }
-
-        public void a()
+        public bool Add(Note note)
         {
-            int a = 4;  
+            note.IsActive = true;
+            note.CreatedDate = DateTime.Now;
+            return _noteDal.Add(note) > 0;
         }
+        public bool Update(Note note)
+        {
+            note.ModifiedDate = DateTime.Now;
+            return _noteDal.Update(note) > 0;
+        }
+        public bool Delete(Note note)
+        {
+            note.IsActive = false;
+            note.ModifiedDate = DateTime.Now;
+            return Update(note);
+        }
+        public Note GetById(int noteID)
+        {
+            return _noteDal.GetByID(noteID);
+        }
+
+        public List<Note> GetNotesByUser(int userID)
+        {
+            return _noteDal.GetAll().Where(a => a.UserID == userID && a.IsActive).ToList();
+        }
+
     }
 }
--- NoteBoard/NoteBoard.BLL/PasswordController.cs
+++ NoteBoard/NoteBoard.BLL/PasswordController.cs
@@ -1,18 +1,48 @@
-﻿using System;
+﻿using NoteBoard.DAL.Repositories;
+using NoteBoard.Model;
+using System;
 using System.Collections.Generic;
-using NoteBoard.DAL.Repositories;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NoteBoard.BLL
 {
-    class PasswordController
+   public class PasswordController
     {
         PasswordDAL _passwordDAL;
         public PasswordController()
         {
             _passwordDAL = new PasswordDAL();
         }
+
+        public bool Add(Password pass)
+        {
+            List<Password> passwords = GetAllByUsers(pass.UserID);
+            passwords = passwords.OrderByDescending(a => a.CreatedDate).Take(3).ToList();
+            if (passwords.FirstOrDefault(a => a.PasswordText == pass.PasswordText) != null)
+            {
+                throw new Exception("Son üç şifreden farklı olmalıdır.");
+            }
+            Delete(passwords.First());
+            pass.IsActive = true;
+            pass.CreatedDate = DateTime.Now;
+            return _passwordDAL.Add(pass) > 0;
+        }
+        List<Password> GetAllByUsers(int userID)
+        {
+            return _passwordDAL.GetAll().Where(a => a.UserID == userID).ToList();
+        }
+        public bool Delete(Password pass)
+        {
+            pass.IsActive = false;
+            pass.ModifiedDate = DateTime.Now;
+            return _passwordDAL.Update(pass) > 0;
+        }
+        public Password GetActivePassword(int userID)
+        {
+            List<Password> allPasswords = GetAllByUsers(userID);
+            return allPasswords.Where(a => a.IsActive).SingleOrDefault();
+        } 
     }
 }
